3e72174502747f0133514434f54f05346c4783d0
Christopher Osborn <z68ozz@gmail.com>
2020-04-20 00:43:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 def batch_badge_creator(attendees)
   badge = []
-  attendees.each {|name| badge.push(badge_maker(name))  
+  attendees.each {|name| badge.push(badge_maker(name))}  
   end
   badge
 end
